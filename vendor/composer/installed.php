531ecf1c656136257dfb5440ef8cc890a7d285bc
Sam Odukoya <samodukoya@bpp.com>
2021-02-27 01:35:06 +0000

    modified the seeders migration

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '9d656c32c3d92333ab00bd87761e8cd1f7245fb3',
+    'name' => 'webpatser/laravel-countries',
+  ),
+  'versions' => 
+  array (
+    'webpatser/laravel-countries' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9d656c32c3d92333ab00bd87761e8cd1f7245fb3',
+    ),
+  ),
+);
